6338d87447aebf1edbc3d0fd1c83008b567eec6f
Kristy <nataripa@gmail.com>
2022-12-12 08:27:44 -0500

    Initial Commit

---
+++ claseSwiftUI/claseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  claseSwiftUIApp.swift
+//  claseSwiftUI
+//
+//  Created by Natalia Riano on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct claseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
